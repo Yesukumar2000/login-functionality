95128eee8bcb24f4ec1d03f7d09f4b96f0d62e44
Yesukumar2000 <121677945+Yesukumar2000@users.noreply.github.com>
2024-06-15 11:49:15 +0530

    Update Axios in  Login.js

--- frontent/client/src/Compoments/Login/Login.js
+++ frontent/client/src/Compoments/Login/Login.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import "./Login.css";
-import {  Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import axios from "axios";
 
 function Login() {
   let emailInputRef = useRef();
@@ -9,98 +10,117 @@ function Login() {
   let dispatch = useDispatch();
   let navigate = useNavigate();
 
- useEffect(()=>{
-  if(localStorage.getItem("token")){
-    validateToken();
-  }
- }, );
+  useEffect(() => {
+    axios.defaults.baseURL = "http://localhost:7999";
+    if (localStorage.getItem("token")) {
+      validateToken();
+      axios.defaults.headers.common['Authorization'] = localStorage.getItem("token")
+    }
+  });
 
-let validateToken= async()=>{
-  let dataToSend = new FormData();
-  dataToSend.append("token",localStorage.getItem("token"));
-  let reqOptions={
-    method:"POST",
-    body:dataToSend
-  }
-  let JSONData = await fetch("http://localhost:7999/validateToken",reqOptions);
-  let JSOData = await JSONData.json();
-  console.log(JSOData);
-  if(JSOData.status === "success"){
-    dispatch({type:"login",data:JSOData.data});
-    navigate("/dashboard");
-  }else{
-    alert(JSOData.msg);
-  }
-}
+  let validateToken = async () => {
+    let dataToSend = new FormData();
+    dataToSend.append("token", localStorage.getItem("token"));
+    let reqOptions = {
+      method: "POST",
+      body: dataToSend,
+    };
+    let JSONData = await fetch(
+      "http://localhost:7999/validateToken",
+      reqOptions
+    );
+    let JSOData = await JSONData.json();
+    console.log(JSOData);
+    if (JSOData.status === "success") {
+      dispatch({ type: "login", data: JSOData.data });
+      navigate("/dashboard");
+    } else {
+      alert(JSOData.msg);
+    }
+  };
 
+  let validateLogin = async () => {
+    let dataToSend = new FormData();
+    dataToSend.append("email", emailInputRef.current.value);
+    dataToSend.append("password", passwordInputRef.current.value);
 
-  // let validateLogin = async()=>{
-    
-  //   let dataToSend = new FormData();
-  //   dataToSend.append("email",emailInputRef.current.value);
-  //   dataToSend.append("password",passwordInputRef.current.value);
-  //   let reqOptions ={
-  //     method:"POST",
-  //     body:dataToSend,
-  //   }
-  //   let JSONData= await fetch("http://localhost:7999/login",reqOptions);
-  //   let JSOData = await JSONData.json();
-  //   console.log(JSOData);
-  //   if(JSOData.status === "success"){
-  //     localStorage.setItem("token",JSOData.data.token);
+    let response = await axios.post("/login", dataToSend);
+    console.log(response);
 
-  //     dispatch({type:"login",data:JSOData.data});
-  //     navigate("/dashboard");
-  //   }else{
-  //     alert(JSOData.msg);
-  //   }
-  // }
-  let validateLogin2 =()=>{
-    console.log("ValidateLogin2")
-    return async()=>{
+    if (response.data.status === "success") {
+      localStorage.setItem("token", response.data.data.token);
+      dispatch({ type: "login", data: response.data.data });
+      navigate("/dashboard");
+    } else {
+      alert(response.data.msg);
+    }
+    // let reqOptions ={
+    //   method:"POST",
+    //   body:dataToSend,
+    // }
+    // let JSONData= await fetch("http://localhost:7999/login",reqOptions);
+    // let JSOData = await JSONData.json();
+    // console.log(JSOData);
+    // if(JSOData.status === "success"){
+    //   localStorage.setItem("token",JSOData.data.token);
+
+    //   dispatch({type:"login",data:JSOData.data});
+    //   navigate("/dashboard");
+    // }else{
+    //   alert(JSOData.msg);
+    // }
+  };
+  let validateLogin2 = () => {
+    console.log("ValidateLogin2");
+    return async () => {
       let dataToSend = new FormData();
-      dataToSend.append("email",emailInputRef.current.value);
-      dataToSend.append("password",passwordInputRef.current.value);
-      let reqOptions ={
-        method:"POST",
-        body:dataToSend,
-      }
-      let JSONData = await fetch("http://localhost:7999/login",reqOptions);
+      dataToSend.append("email", emailInputRef.current.value);
+      dataToSend.append("password", passwordInputRef.current.value);
+
+      let reqOptions = {
+        method: "POST",
+        body: dataToSend,
+      };
+      let JSONData = await fetch("http://localhost:7999/login", reqOptions);
       let JSOData = await JSONData.json();
       console.log(JSOData);
-      if(JSOData.status === "success"){
-        localStorage.setItem("token",JSOData.data.token);
-  
-        dispatch({type:"login",data:JSOData.data});
+      if (JSOData.status === "success") {
+        localStorage.setItem("token", JSOData.data.token);
+
+        dispatch({ type: "login", data: JSOData.data });
         navigate("/dashboard");
-      }else{
+      } else {
         alert(JSOData.msg);
       }
-    }
-
-
-  } 
+    };
+  };
   return (
     <div className="container">
-    <form>
-      <h2>Login</h2>
-      <div>
-        <label>Email</label>
-        <input ref={emailInputRef} type="email" placeholder="Email" />
+      <form>
+        <h2>Login</h2>
+        <div>
+          <label>Email</label>
+          <input ref={emailInputRef} type="email" placeholder="Email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input ref={passwordInputRef} type="text" placeholder="Password" />
+        </div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              validateLogin();
+               dispatch(validateLogin2());
+            }}>
+            Login
+          </button>
+        </div>
+      </form>
+      <div className="Link">
+        <Link to="/signup">Sign Up</Link>
       </div>
-      <div>
-        <label>Password</label>
-        <input ref={passwordInputRef} type="text" placeholder="Password" />
-      </div>
-      <div>
-        <button type="button" onClick={()=>{
-          // validateLogin
-             dispatch(validateLogin2());
-          }}>Login</button>
-      </div>
-    </form>
-    <div className="Link"><Link to='/signup'>Sign Up</Link></div>
-  </div>
+    </div>
   );
 }
 
